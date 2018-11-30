1c78cb01788f0724e448755ec05d252e2b743a34
NathanielYoung <young.nathaniel95@gmail.com>
2018-11-29 22:45:31 -0700

    Redux set up

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,7 +1,24 @@
 import { combineReducers } from 'redux';
+import { ADD_ARTICLE, CHANGE_STATE } from "./constants/action-types";
 
-const rootReducer = combineReducers({
-  state: (state = {}) => state
-});
+const initialState = {
+    testValue : 'hi im a value',
+}
 
-export default rootReducer;
+const rootReducer = (state = initialState, action) => {
+    switch (action.type) {
+      case ADD_ARTICLE:
+        state.articles.push(action.payload);
+        return state;
+      case CHANGE_STATE:
+          const changedState = action.payload
+          return {
+              ...state,
+              ...changedState
+          }
+      default:
+        return state;
+    }
+  };
+  
+  export default rootReducer;
