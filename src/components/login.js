413690de283d85c042d35ceefdf6065b00527037
NathanielYoung <young.nathaniel95@gmail.com>
2019-01-11 13:07:11 -0700

    complete fix

---
+++ src/components/login.js
@@ -0,0 +1,72 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import * as actions from '../reducers/actions';
+import Footer from './parts/footer';
+import { Link } from 'react-router-dom';
+import ContainedButtons from '../components/parts/button';
+
+class Login extends Component {
+
+  state = {
+    mysqlDB : [],
+    user : {
+      user_id: '',
+      user_fname: '',
+      user_lname: '',
+      user_password: '',
+      user_code: ''
+    }
+  }
+
+  componentDidMount() {
+    this.getProducts();
+  }
+
+  getProducts = _ => {
+    fetch('https://ww1simdb.herokuapp.com/sql')
+    .then(response => response.json())
+    .then(response => this.setState({ mysqlDB : response.data }))
+    .catch(err => console.error(err))
+  }
+
+  addProduct = _ => {
+    const { product } = this.state;
+    fetch(`https://ww1simdb.herokuapp.com/login/add?name=${this.state.users.name}&price=${this.state.users.password}`)
+    .then(this.getProducts)
+    .catch(err => console.error(err))
+  }
+
+  renderProduct = ({user_id, user_fname, user_lname, user_password, user_code}) => <div key={user_id}>{`${user_id} ${user_fname} ${user_lname} ${user_password} ${user_code}`}</div>
+
+  login() { 
+    const mysql = require("mysql");
+    // First you need to create a connection to the db
+
+    console.log(document.getElementById('fname').value + ' : ' + document.getElementById('pass').value);
+    
+  }
+
+  render() {
+    return (
+      <div className='login'>
+      <form>
+        Name: <input type="text" name="fname" id="fname" onChange={e => this.setState({ users: { ...this.state.users, name: e.target.value }})}/>
+        Password: <input type="password" id="pass"  onChange={e => this.setState({ users: { ...this.state.users, password: e.target.value }})}/>
+        <Link to="/login">
+          <input type="submit" value="submit" onClick={() => this.addProduct()}/>
+        </Link>
+      </form>
+
+      {this.state.mysqlDB.map(this.renderProduct)}
+
+      <Footer />
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = (state) => {
+    return state
+}
+Login = connect(mapStateToProps, actions)(Login);
+export default Login;
