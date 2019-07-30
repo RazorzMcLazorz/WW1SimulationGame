613b1ce5c2ec92c1c1e7d01ec531e3fa22318d63
Nathaniel Young <young.nathaniel95@gmail.com>
2019-07-29 19:44:12 -0600

    player page being setup

--- src/components/players.js
+++ src/components/players.js
@@ -1,12 +1,23 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import * as actions from '../reducers/actions'
+import NavBar from './parts/navBar'
+import Footer from './parts/footer'
 
 class Players extends Component {
+
+  players(country) {
+    
+  }
 
   render() {
     return (
       <div className='players'>
+        <NavBar/>
+
+          {this.players(this.props.countrySelected)}
+
+        <Footer />
       </div>
     );
   }
